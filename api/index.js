30ed4a0099f88d95f1d7effa1bcf322621f80852
Jubair Ahmed Nabin <ahmednabin.csecuet@gmail.com>
2023-02-06 06:22:14 +0600

    Set up adding and get msg functionality in backend

--- api/index.js
+++ api/index.js
@@ -8,6 +8,7 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const conversationRoute = require("./routes/conversations");
+const messageRoute = require("./routes/messages");
 
 dotenv.config();
 
@@ -23,6 +24,7 @@ app.use(morgan("common"));
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
 
 app.listen(8800, () => {
     console.log("Backend server is running!");
